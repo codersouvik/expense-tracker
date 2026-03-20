c2bf2272e030db21e50397fb98e9c6a13703558d
codersouvik <98273213+codersouvik@users.noreply.github.com>
2026-03-20 18:20:17 +0530

    made some changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     <Route path="/register" element={<Register/>}/>
     <Route path="/dashboard" element={<ProtectedRoute><Dashboard/></ProtectedRoute>}/>
     <Route path="/forgot-password" element={<ForgotPassword/>}/>
-    <Route path="/reset-password" element={<ResetPassword/>}/>
+    <Route path="/reset-password/:token" element={<ResetPassword/>}/>
       <Route path="*" element={<Navigate to="/" />} />
    </Routes>  
    
