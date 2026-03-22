64f5f46ecd7c4e8d1dc306ba53c64391019c37a5
codersouvik <98273213+codersouvik@users.noreply.github.com>
2026-03-22 12:59:09 +0530

    Forgot password logic changed

--- Backend/controllers/authcontroller.js
+++ Backend/controllers/authcontroller.js
@@ -98,7 +98,9 @@ exports.ForgotPassword = async(req,res)=>{
     console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
 
      const transporter  = nodemailer.createTransport({
-      service:"gmail",
+       host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
       auth:{
         user:process.env.EMAIL_USER,
         pass:process.env.EMAIL_PASS,
