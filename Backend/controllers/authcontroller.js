0421e31aec7663199b8f54a8ebeb1d83822b116a
codersouvik <98273213+codersouvik@users.noreply.github.com>
2026-03-22 13:09:18 +0530

    Logic Changed

--- Backend/controllers/authcontroller.js
+++ Backend/controllers/authcontroller.js
@@ -101,10 +101,15 @@ exports.ForgotPassword = async(req,res)=>{
        host: "smtp.gmail.com",
   port: 587,
   secure: false,
+   requireTLS: true,
       auth:{
         user:process.env.EMAIL_USER,
         pass:process.env.EMAIL_PASS,
-      }
+      },
+      connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+  family: 4,
      })
      console.log("Transporter created");
 
