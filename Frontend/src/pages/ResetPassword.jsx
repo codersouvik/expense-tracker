5059833eed443855a956b052801df9a8d3d28995
codersouvik <98273213+codersouvik@users.noreply.github.com>
2026-03-22 11:27:04 +0530

    Logic Changed

--- Frontend/src/pages/ResetPassword.jsx
+++ Frontend/src/pages/ResetPassword.jsx
@@ -1,47 +1,46 @@
-import {useState,useContext} from 'react';
-import {AuthContext} from '../context/AuthContext';
-import {useNavigate,useParams} from 'react-router-dom'
+import { useState, useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import { useNavigate, useParams } from 'react-router-dom'
 
-const ResetPassword =()=>{
+const ResetPassword = () => {
 
-    const {resetPassword} = useContext(AuthContext);
-    const {token} = useParams();
+    const { resetPassword } = useContext(AuthContext);
+    const { token } = useParams();
     const navigate = useNavigate();
 
-    const[password,Setpassword] = useState("");
-    const [message,Setmessage] = useState("");
+    const [password, Setpassword] = useState("");
+    const [message, Setmessage] = useState("");
 
-    const HandelSubmit = async (e)=>{
+    const HandelSubmit = async (e) => {
         e.preventDefault();
 
-        try{
-               const res = await resetPassword(token,password);
-               Setmessage(res.message);
+        try {
+            const res = await resetPassword(token, password);
+            Setmessage(res.message);
 
-               setTimeout(()=>{
-                  navigate("/login")
-               },2000)
+            setTimeout(() => {
+                navigate("/login")
+            }, 2000)
         }
-        catch(err)
-        {
+        catch (err) {
             Setmessage(err.message)
         }
-        
+
     }
 
-    return(
+    return (
         <div className="auth-container">
             <div className="auth-card">
                 <h2>Reset Password</h2>
                 <form onSubmit={HandelSubmit}>
                     <input
-  type="password"
-  placeholder="Enter New Password"
-  value={password}
-  onChange={(e) => Setpassword(e.target.value)}
-  required
-  autoComplete="new-password"
-/>
+                        type="password"
+                        placeholder="Enter New Password"
+                        value={password}
+                        onChange={(e) => Setpassword(e.target.value)}
+                        required
+                        autoComplete="new-password"
+                    />
                     <button type="submit">Reset Password</button>
 
                 </form>
